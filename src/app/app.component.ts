3dc9375b68827e363739f5947b30a1977d252323
Isuru Ashinsana <isuruashinsana@gmail.com>
2023-12-28 00:29:40 +0530

    create cart.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Cart} from "./models/cart.model";
+import {CartService} from "./service/cart.service";
 
 @Component({
   selector: 'app-root',
   template: `
-        <app-header/>
+        <app-header [cart]="cart"/>
         <router-outlet/>
   `,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'store';
+export class AppComponent implements OnInit{
+  cart:Cart={
+    items:[]
+  }
+  constructor(private cartService:CartService) {
+  }
+  ngOnInit() {
+    this.cartService.cart.subscribe((_cart)=>{
+      this.cart=_cart;
+    })
+  }
 }
